4dadf6d202af44b0eaca54ebe6c0c031bb626c45
Eric Kuck <eric@spothero.com>
2014-01-16 12:12:19 -0600

    Added Crashlytics

--- RyteBytes/src/main/java/com/myrytebytes/rytebytes/RyteBytesApplication.java
+++ RyteBytes/src/main/java/com/myrytebytes/rytebytes/RyteBytesApplication.java
@@ -2,6 +2,7 @@ package com.myrytebytes.rytebytes;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.myrytebytes.datamanagement.UserController;
 import com.myrytebytes.remote.ApiInterface;
 import com.myrytebytes.widget.NetworkImageView;
@@ -12,10 +13,10 @@ public class RyteBytesApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+        Crashlytics.start(this);
 		Parse.initialize(this, "zaZmkcjbGLCrEHagb8uJPt5TKyiFgCg9WffA6c6M", "DltIu9MSxC9k1ly58gpdpXMkGlPI6KkfSeTkjwYa");
 		ApiInterface.init(this);
 		NetworkImageView.init(this);
         UserController.init(this);
 	}
-
 }
