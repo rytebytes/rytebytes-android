6ec493d17655ea0d532465da5ed7c6f315726b0c
Eric Kuck <eric@spothero.com>
2014-05-20 12:20:15 -0500

    Minor refactor

--- RyteBytes/src/main/java/com/myrytebytes/widget/NetworkImageView.java
+++ RyteBytes/src/main/java/com/myrytebytes/widget/NetworkImageView.java
@@ -5,7 +5,6 @@ import android.graphics.Bitmap;
 import android.support.v4.util.LruCache;
 import android.util.AttributeSet;
 
-import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.ImageLoader.ImageCache;
 import com.android.volley.toolbox.Volley;
@@ -13,7 +12,6 @@ import com.myrytebytes.remote.OkHttpStack;
 
 public class NetworkImageView extends com.android.volley.toolbox.NetworkImageView {
 
-	private static RequestQueue sRequestQueue;
 	private static ImageLoader sImageLoader;
 
 	public NetworkImageView(Context context) {
@@ -29,8 +27,7 @@ public class NetworkImageView extends com.android.volley.toolbox.NetworkImageVie
 	}
 
 	public static void init(Context context) {
-		sRequestQueue = Volley.newRequestQueue(context, new OkHttpStack());
-		sImageLoader = new ImageLoader(sRequestQueue, new ImageCache() {
+		sImageLoader = new ImageLoader(Volley.newRequestQueue(context, new OkHttpStack()), new ImageCache() {
 			private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(10);
 
 			@Override
